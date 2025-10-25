0eed672b785b5d012005871602b5a7a3681d918a
dgl1797 <digiacomoluca1797@gmail.com>
2025-10-25 11:12:50 +0200

    "revert"

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
-Remove-Item .\MySQL -Recurse -Force
-Remove-Item .\Mongo -Recurse -Force
-Remove-Item .\Redis -Recurse -Force
-mkdir .\MySQL
-mkdir .\Mongo
+if (Test-Path -path ".\MySQL\data"){ Remove-Item .\MySQL\data -Recurse -Force }
+if (Test-Path -path ".\Mongo\data"){ Remove-Item .\Mongo -Recurse -Force }
+if (Test-Path -path ".\Redis"){ Remove-Item .\Redis -Recurse -Force }
+mkdir .\MySQL\data
+mkdir .\Mongo\data
 mkdir .\Redis
 docker-compose up -d --build
